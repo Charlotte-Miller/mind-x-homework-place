6fdf5cba9caf8fdcb7409b01111851ac82bd90a6
Chungnh98f <59239961+Chungnh98f@users.noreply.github.com>
2020-03-19 23:40:33 +0700

    add button listener function

--- khóa-1-code-for-everyone-js/homework-1/main.js
+++ khóa-1-code-for-everyone-js/homework-1/main.js
@@ -1,21 +1,18 @@
 // Program calculate area of circle
-const radius = Number(prompt('Give me your radius.'));
-const circleArea = radius*radius*Math.PI;
+const radius = Number(prompt("Give me your radius."));
+const circleArea = radius * radius * Math.PI;
 function calCircleArea() {
-    alert(`The circle area is: ${circleArea}`);
-} 
-
-
-
+  alert(`The circle area is: ${circleArea}`);
+}
+const circleButton = document.getElementById('circle');
+circleButton.addEventListener('click', calCircleArea);
 
 
 // Program convert Celcius into Fahrenheit
-const celcius = Number(prompt('How cold is the weather?'));
-const fahrenheit = 32 + celcius*1.8;
+const celcius = Number(prompt("How cold is the weather?"));
+const fahrenheit = 32 + celcius * 1.8;
 function calFah() {
-    alert(`The weather is: ${fahrenheit} (F)`)
+  alert(`The weather is: ${fahrenheit} (F)`);
 }
-
-
-calCircleArea();
-calFah();
+const celciusButton = document.getElementById('celcius');
+celciusButton.addEventListener('click', calFah);
