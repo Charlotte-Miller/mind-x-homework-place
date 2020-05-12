93d2940ecedd15a5a0df9a23212e64b9f45aea9e
Chungnh98f <chungnguyen011298@gmail.com>
2020-05-12 19:37:41 +0700

    upload exercise session 4

--- khóa-1-code-for-everyone-js/corona-wfh-2/main.js
+++ khóa-1-code-for-everyone-js/corona-wfh-2/main.js
@@ -9,13 +9,13 @@ const button7 = document.getElementById("button7");
 const button8 = document.getElementById("button8");
 const button9 = document.getElementById("button9");
 
-// defince functional func
+// define functional func
 function fib(num) {
-    if(num === 1 || num ===2) {
-        return 1;
-    } else {
-        return (fib(num - 1) + fib(num -2));
-    }
+  if (num === 1 || num === 2) {
+    return 1;
+  } else {
+    return fib(num - 1) + fib(num - 2);
+  }
 }
 
 // define click func
@@ -46,7 +46,7 @@ const primeCheck = function () {
     result = false;
   } else if (prime === 2) {
     result = true;
-  } else if (prime >= 3){
+  } else if (prime >= 3) {
     for (let count = 2; count < prime; count++) {
       if (prime % count === 0) {
         result = false;
@@ -87,81 +87,81 @@ const primePrint = function () {
   }
 };
 
-const findFibonacci = function() {
-    let number = Number(prompt('Fill in a number (number > 0):'));
-    if (number <= 0) {
-        console.log('Can not find Fibonacci');
-    } else if(number > 0) {
-       console.log(`fibonacci: ${fib(number)}`);
+const findFibonacci = function () {
+  let number = Number(prompt("Fill in a number (number > 0):"));
+  if (number <= 0) {
+    console.log("Can not find Fibonacci");
+  } else if (number > 0) {
+    console.log(`fibonacci: ${fib(number)}`);
+  }
+};
+
+const calSumPositive = function () {
+  let numberArray = [0];
+  while (numberArray[numberArray.length - 1] >= 0) {
+    const number = Number(prompt("Fill a number:"));
+    numberArray.push(number);
+  }
+  console.log(
+    numberArray.reduce((a, b) => a + b) - numberArray[numberArray.length - 1]
+  );
+};
+
+const symmetricCheck = function () {
+  const string = prompt("fill in a string:");
+  let result = false;
+  for (let count = 0; count < Math.floor(string.length / 2); count++) {
+    if (string[count] !== string[string.length - 1 - count]) {
+      result = false;
+      break;
+    } else {
+      result = true;
     }
-}
+  }
+  console.log(result);
+};
 
-const calSumPositive = function() {
-    let numberArray = [0]
-    while (numberArray[numberArray.length - 1] >= 0){
-        const number = Number(prompt('Fill a number:'));
-        numberArray.push(number);
-    }
-    console.log(numberArray.reduce((a, b) => a + b) - numberArray[numberArray.length - 1]);
-}
+const drawRecArea = function () {
+  let height = Number(prompt("Fill height of rectangle:"));
+  let width = Number(prompt("Fill width of rectangle:"));
+  let string = "";
+  for (let column = 1; column <= height; column++) {
+    string += "  *";
+  }
+  for (let line = 1; line <= width; line++) {
+    console.log(string);
+    console.log("");
+  }
+};
 
-const symmetricCheck = function() {
-    const string = prompt('fill in a string:');
-    let result = false;
-        for(let count = 0; count < Math.floor(string.length/2); count++){
-            if(string[count] !== string[string.length - 1 - count]) {
-                result = false;
-                break;
-            } else {
-                result = true;
-            }
-        };
-    console.log(result);
-}
+const calDogAndChicken = function () {
+  let a1 = 1;
+  let b1 = 1;
+  let c1 = 36;
+  let a2 = 2;
+  let b2 = 4;
+  let c2 = 100;
 
-const drawRecArea = function() {
-    let height = Number(prompt('Fill height of rectangle:'));
-    let width = Number(prompt('Fill width of rectangle:'));
-    let string = '';
-    for(let column = 1; column <= height; column++){
-        string += '  *';
-    }
-    for(let line = 1; line <= width; line++){
-        console.log(string);
-        console.log('');
-    }
-}
+  const dog = (c2 - (c1 * a2) / a1) / (b2 - (b1 * a2) / a1);
+  const chicken = c1 / a1 - (b1 * dog) / a1;
+  console.log(`chicken: ${chicken}
+dog: ${dog}`);
+};
 
-const calDogAndChicken = function() {
-    let a1 = 1;
-    let b1 = 1;
-    let c1 = 36;
-    let a2 = 2;
-    let b2 = 4;
-    let c2 = 100;
-    
-    const dog = (c2 - (c1*a2)/a1)/(b2-(b1*a2)/a1);
-    const chicken = c1/a1 - (b1*dog)/a1;
-    console.log(`chicken: ${chicken}
-dog: ${dog}`)
-}
+const findSolution = function () {
+  let a1 = Number(prompt("a1:"));
+  let b1 = Number(prompt("b1:"));
+  let c1 = Number(prompt("c1:"));
+  let a2 = Number(prompt("a2:"));
+  let b2 = Number(prompt("b2:"));
+  let c2 = Number(prompt("c2:"));
 
-const findSolution = function() {
-    let a1 = Number(prompt('a1:'));
-    let b1 = Number(prompt('b1:'));
-    let c1 = Number(prompt('c1:'));
-    let a2 = Number(prompt('a2:'));
-    let b2 = Number(prompt('b2:'));
-    let c2 = Number(prompt('c2:'));
+  const y = (c2 - (c1 * a2) / a1) / (b2 - (b1 * a2) / a1);
+  const x = c1 / a1 - (b1 * y) / a1;
 
-    const y = (c2 - (c1*a2)/a1)/(b2-(b1*a2)/a1);
-    const x = c1/a1 - (b1*y)/a1;
-
-        console.log(`x: ${x}
-y: ${y}`)
-
-
-}
+  console.log(`x: ${x}
+y: ${y}`);
+};
 // define click ac2ion
 
 button1.addEventListener("click", calSumOddAndEven);
